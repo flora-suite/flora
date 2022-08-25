Lichtblick-Suite/lichtblick

commit caaf4501a21674409587a0d35c45a5a71cac7346

Improve data source info panel performance (#4238)
This uses refs to bypass react entirely for rapidly updating topic stats.

packages/studio-base/src/components/DataSourceInfoView.tsx
@@ -3,20 +3,21 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { Skeleton, Typography } from "@mui/material";
-import { useDebounce } from "use-debounce";
+import { MutableRefObject, useEffect, useRef } from "react";
 
 import { Time } from "@foxglove/rostime";
-import Duration from "@foxglove/studio-base/components/Duration";
 import {
   MessagePipelineContext,
   useMessagePipeline,
 } from "@foxglove/studio-base/components/MessagePipeline";
 import Stack from "@foxglove/studio-base/components/Stack";
 import Timestamp from "@foxglove/studio-base/components/Timestamp";
+import { useAppTimeFormat } from "@foxglove/studio-base/hooks";
 import { subtractTimes } from "@foxglove/studio-base/players/UserNodePlayer/nodeTransformerWorker/typescript/userUtils/time";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
+import { formatDate, formatDuration } from "@foxglove/studio-base/util/formatTime";
 
-import { MultilineMiddleTruncate } from "../MultilineMiddleTruncate";
+import { MultilineMiddleTruncate } from "./MultilineMiddleTruncate";
 
 const selectStartTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.startTime;
 const selectEndTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.endTime;
@@ -24,13 +25,13 @@ const selectPlayerName = (ctx: MessagePipelineContext) => ctx.playerState.name;
 const selectPlayerPresence = ({ playerState }: MessagePipelineContext) => playerState.presence;
 
 function DataSourceInfoContent(props: {
-  duration?: Time;
-  endTime?: Time;
+  durationRef: MutableRefObject<ReactNull | HTMLDivElement>;
+  endTimeRef: MutableRefObject<ReactNull | HTMLDivElement>;
   playerName?: string;
   playerPresence: PlayerPresence;
   startTime?: Time;
 }): JSX.Element {
-  const { duration, endTime, playerName, playerPresence, startTime } = props;
+  const { durationRef, endTimeRef, playerName, playerPresence, startTime } = props;
 
   return (
     <Stack gap={1.5} paddingX={2} paddingBottom={2}>
@@ -72,10 +73,8 @@ function DataSourceInfoContent(props: {
         </Typography>
         {playerPresence === PlayerPresence.INITIALIZING ? (
           <Skeleton animation="wave" width="50%" />
-        ) : endTime ? (
-          <Timestamp horizontal time={endTime} />
         ) : (
-          <Typography variant="inherit" color="text.secondary">
+          <Typography variant="inherit" ref={endTimeRef}>
             &mdash;
           </Typography>
         )}
@@ -87,10 +86,8 @@ function DataSourceInfoContent(props: {
         </Typography>
         {playerPresence === PlayerPresence.INITIALIZING ? (
           <Skeleton animation="wave" width={100} />
-        ) : duration ? (
-          <Duration duration={duration} />
         ) : (
-          <Typography variant="inherit" color="text.secondary">
+          <Typography variant="inherit" ref={durationRef}>
             &mdash;
           </Typography>
         )}
@@ -101,27 +98,45 @@ function DataSourceInfoContent(props: {
 
 const MemoDataSourceInfoContent = React.memo(DataSourceInfoContent);
 
-function DataSourceInfo(): JSX.Element {
+const EmDash = "\u2014";
+
+export function DataSourceInfoView(): JSX.Element {
   const startTime = useMessagePipeline(selectStartTime);
   const endTime = useMessagePipeline(selectEndTime);
   const playerName = useMessagePipeline(selectPlayerName);
   const playerPresence = useMessagePipeline(selectPlayerPresence);
+  const durationRef = useRef<HTMLDivElement>(ReactNull);
+  const endTimeRef = useRef<HTMLDivElement>(ReactNull);
+  const { formatTime } = useAppTimeFormat();
 
-  const [debouncedTimes] = useDebounce(
-    { endTime, duration: startTime && endTime ? subtractTimes(endTime, startTime) : undefined },
-    100,
-    { leading: true, maxWait: 100 },
-  );
+  // We bypass react and update the DOM elements directly for better performance here.
+  useEffect(() => {
+    if (durationRef.current) {
+      const duration = endTime && startTime ? subtractTimes(endTime, startTime) : undefined;
+      if (duration) {
+        const durationStr = formatDuration(duration);
+        durationRef.current.innerText = durationStr;
+      } else {
+        durationRef.current.innerText = EmDash;
+      }
+    }
+    if (endTimeRef.current) {
+      if (endTime) {
+        const date = formatDate(endTime, undefined);
+        endTimeRef.current.innerText = `${date} ${formatTime(endTime)}`;
+      } else {
+        endTimeRef.current.innerText = EmDash;
+      }
+    }
+  }, [endTime, formatTime, startTime]);
 
   return (
     <MemoDataSourceInfoContent
-      duration={debouncedTimes.duration}
-      endTime={debouncedTimes.endTime}
+      durationRef={durationRef}
+      endTimeRef={endTimeRef}
       playerName={playerName}
       playerPresence={playerPresence}
       startTime={startTime}
     />
   );
 }
-
-export { DataSourceInfo };

packages/studio-base/src/components/DataSourceSidebar/DataSourceSidebar.tsx
@@ -22,7 +22,7 @@ import { SidebarContent } from "@foxglove/studio-base/components/SidebarContent"
 import Stack from "@foxglove/studio-base/components/Stack";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
 
-import { DataSourceInfo } from "./DataSourceInfo";
+import { DataSourceInfoView } from "../DataSourceInfoView";
 import { ProblemsList } from "./ProblemsList";
 import { TopicList } from "./TopicList";
 import helpContent from "./help.md";
@@ -128,7 +128,7 @@ export default function DataSourceSidebar(props: Props): JSX.Element {
       ].filter(Boolean)}
     >
       <Stack fullHeight>
-        <DataSourceInfo />
+        <DataSourceInfoView />
         {playerPresence !== PlayerPresence.NOT_PRESENT && (
           <>
             <Divider />

packages/studio-base/src/components/DataSourceSidebar/TopicList.stories.tsx
@@ -1,0 +1,59 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { Story } from "@storybook/react";
+
+import MockMessagePipelineProvider from "@foxglove/studio-base/components/MessagePipeline/MockMessagePipelineProvider";
+import { TopicStats } from "@foxglove/studio-base/players/types";
+
+import { TopicList } from "./TopicList";
+
+function Wrapper(StoryFn: Story): JSX.Element {
+  return (
+    <MockMessagePipelineProvider
+      topics={[
+        {
+          name: "/topic_1",
+          datatype: "std_msgs/String",
+        },
+        {
+          name: '"/topic_2"',
+          datatype: "std_msgs/String",
+        },
+      ]}
+      topicStats={
+        new Map<string, TopicStats>([
+          [
+            "/topic_1",
+            {
+              numMessages: 1234,
+              firstMessageTime: { sec: 1, nsec: 0 },
+              lastMessageTime: { sec: 2, nsec: 0 },
+            },
+          ],
+          [
+            '"/topic_2"',
+            {
+              numMessages: 3456,
+              firstMessageTime: { sec: 1, nsec: 0 },
+              lastMessageTime: { sec: 2, nsec: 0 },
+            },
+          ],
+        ])
+      }
+    >
+      <StoryFn />
+    </MockMessagePipelineProvider>
+  );
+}
+
+export default {
+  component: TopicList,
+  title: "components/TopicList",
+  decorators: [Wrapper],
+};
+
+export function Default(): JSX.Element {
+  return <TopicList />;
+}

packages/studio-base/src/components/DataSourceSidebar/TopicList.tsx
@@ -18,11 +18,9 @@ import {
   TypographyProps,
 } from "@mui/material";
 import { Fzf, FzfResultItem } from "fzf";
-import { cloneDeep } from "lodash";
 import { useMemo, useState } from "react";
-import { useDebounce } from "use-debounce";
 
-import { areEqual, subtract as subtractTimes, Time, toSec } from "@foxglove/rostime";
+import { DirectTopicStatsUpdater } from "@foxglove/studio-base/components/DirectTopicStatsUpdater";
 import {
   MessagePipelineContext,
   useMessagePipeline,
@@ -108,90 +106,57 @@ const StyledListItem = muiStyled(ListItem, { skipSx: true })(({ theme }) => ({
 }));
 
 const EMPTY_TOPICS: Topic[] = [];
-const EMPTY_TOPIC_STATS = new Map<string, TopicStats>();
 
 const selectPlayerPresence = ({ playerState }: MessagePipelineContext) => playerState.presence;
-const selectStartTime = ({ playerState }: MessagePipelineContext) =>
-  playerState.activeData?.startTime;
-const selectEndTime = ({ playerState }: MessagePipelineContext) => playerState.activeData?.endTime;
 
 const selectTopics = (ctx: MessagePipelineContext) =>
   ctx.playerState.activeData?.topics ?? EMPTY_TOPICS;
 
-const selectTopicStats = (ctx: MessagePipelineContext) =>
-  ctx.playerState.activeData?.topicStats ?? EMPTY_TOPIC_STATS;
-
-const messageFrequency = (topic: TopicWithStats, duration: Time | undefined) => {
-  const { numMessages, firstMessageTime, lastMessageTime } = topic;
-
-  if (numMessages == undefined || numMessages < 2) {
-    // Not enough messages to calculate a frequency
-    return undefined;
-  }
-  if (firstMessageTime == undefined || lastMessageTime == undefined) {
-    if (duration == undefined) {
-      return undefined;
-    }
-
-    // Message count but no timestamps, use the full connection duration
-    const durationSec = toSec(duration);
-    if (durationSec === 0) {
-      return undefined;
-    }
-    const value = numMessages / durationSec;
-    const digits = value >= 1000 ? 0 : value >= 100 ? 1 : 2;
-    return `${value.toFixed(digits)} Hz`;
-  }
-  if (areEqual(firstMessageTime, lastMessageTime)) {
-    // Not enough time span to calculate a frequency
-    return undefined;
-  }
-  const topicDurationSec = toSec(subtractTimes(lastMessageTime, firstMessageTime));
-
-  const value = (numMessages - 1) / topicDurationSec;
-  const digits = value >= 1000 ? 0 : value >= 100 ? 1 : 2;
-  return `${value.toFixed(digits)} Hz`;
-};
-
 function TopicListItem({
-  item,
-  messageCount,
-  messageHz,
+  topic,
   positions,
 }: {
-  item: TopicWithStats;
-  messageCount?: number;
-  messageHz?: string;
+  topic: Topic;
   positions: Set<number>;
 }): JSX.Element {
   return (
     <StyledListItem
       divider
-      key={item.name}
+      key={topic.name}
       secondaryAction={
-        (messageCount != undefined || messageHz != undefined) && (
-          <Stack style={{ textAlign: "right" }}>
-            <Typography variant="caption" color="text.secondary">
-              {messageCount ?? "–"}
-            </Typography>
-            <Typography variant="caption" color="text.secondary">
-              {messageHz ?? "–"}
-            </Typography>
-          </Stack>
-        )
+        <Stack style={{ textAlign: "right" }}>
+          <Typography
+            variant="caption"
+            component="div"
+            color="text.secondary"
+            data-topic={topic.name}
+            data-topic-stat="count"
+          >
+            &mdash;
+          </Typography>
+          <Typography
+            variant="caption"
+            component="div"
+            color="text.secondary"
+            data-topic={topic.name}
+            data-topic-stat="frequency"
+          >
+            &mdash;
+          </Typography>
+        </Stack>
       }
     >
       <ListItemText
-        primary={<HighlightChars str={item.name} indices={positions} />}
-        primaryTypographyProps={{ noWrap: true, title: item.name }}
+        primary={<HighlightChars str={topic.name} indices={positions} />}
+        primaryTypographyProps={{ noWrap: true, title: topic.name }}
         secondary={
-          <HighlightChars str={item.datatype} indices={positions} offset={item.name.length + 1} />
+          <HighlightChars str={topic.datatype} indices={positions} offset={topic.name.length + 1} />
         }
         secondaryTypographyProps={{
           variant: "caption",
           fontFamily: fonts.MONOSPACE,
           noWrap: true,
-          title: item.datatype,
+          title: topic.datatype,
         }}
         style={{ marginRight: "48px" }}
       />
@@ -205,43 +170,18 @@ export function TopicList(): JSX.Element {
   const [filterText, setFilterText] = useState<string>("");
 
   const playerPresence = useMessagePipeline(selectPlayerPresence);
-  const startTime = useMessagePipeline(selectStartTime);
-  const endTime = useMessagePipeline(selectEndTime);
   const topics = useMessagePipeline(selectTopics);
-  const topicStats = useMessagePipeline(selectTopicStats);
-  const items: TopicWithStats[] = useMemo(
-    () =>
-      topics.map((topic) => {
-        const stats = topicStats.get(topic.name);
-        return { ...topic, ...stats };
-      }),
-    [topics, topicStats],
-  );
 
-  // Clone deep is necessary here because players mutate the stats directly and
-  // break memoization.
-  const [debouncedData] = useDebounce(
-    {
-      items: cloneDeep(items),
-      duration:
-        endTime != undefined && startTime != undefined
-          ? subtractTimes(endTime, startTime)
-          : undefined,
-    },
-    1000,
-    { leading: true, maxWait: 1000 },
-  );
-
-  const filteredTopics: FzfResultItem<TopicWithStats>[] = useMemo(
+  const filteredTopics: FzfResultItem<Topic>[] = useMemo(
     () =>
       filterText
-        ? new Fzf(debouncedData.items, {
+        ? new Fzf(topics, {
             fuzzy: filterText.length > 2 ? "v2" : false,
             sort: true,
             selector: (item) => `${item.name}|${item.datatype}`,
           }).find(filterText)
-        : debouncedData.items.map((item) => topicToFzfResult(item)),
-    [filterText, debouncedData.items],
+        : topics.map((item) => topicToFzfResult(item)),
+    [filterText, topics],
   );
 
   if (playerPresence === PlayerPresence.ERROR) {
@@ -313,19 +253,8 @@ export function TopicList(): JSX.Element {
 
       {filteredTopics.length > 0 ? (
         <List key="topics" dense disablePadding>
-          {filteredTopics.map(({ item, positions }) => {
-            const messageCount = item.numMessages;
-            const messageHz = messageFrequency(item, debouncedData.duration);
-
-            return (
-              <MemoTopicListItem
-                key={item.name}
-                item={item}
-                messageCount={messageCount}
-                messageHz={messageHz}
-                positions={positions}
-              />
-            );
+          {filteredTopics.map(({ item: topic, positions }) => {
+            return <MemoTopicListItem key={topic.name} topic={topic} positions={positions} />;
           })}
         </List>
       ) : (
@@ -349,6 +278,7 @@ export function TopicList(): JSX.Element {
           )}
         </Stack>
       )}
+      <DirectTopicStatsUpdater interval={6} />
     </>
   );
 }

packages/studio-base/src/components/DirectTopicStatsUpdater.tsx
@@ -1,0 +1,106 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import { useCallback, useEffect, useLayoutEffect, useRef } from "react";
+import { useLatest } from "react-use";
+
+import { areEqual, Time, toSec } from "@foxglove/rostime";
+import {
+  MessagePipelineContext,
+  useMessagePipeline,
+} from "@foxglove/studio-base/components/MessagePipeline";
+import { subtractTimes } from "@foxglove/studio-base/players/UserNodePlayer/nodeTransformerWorker/typescript/userUtils/time";
+import { TopicStats } from "@foxglove/studio-base/players/types";
+
+const EM_DASH = "\u2014";
+const EMPTY_TOPIC_STATS = new Map<string, TopicStats>();
+
+function formatItemFrequency(
+  numMessages: number,
+  firstMessageTime: undefined | Time,
+  lastMessageTime: undefined | Time,
+  duration: Time,
+) {
+  if (firstMessageTime == undefined || lastMessageTime == undefined) {
+    // Message count but no timestamps, use the full connection duration
+    return `${(numMessages / toSec(duration)).toFixed(2)} Hz`;
+  }
+  if (numMessages < 2 || areEqual(firstMessageTime, lastMessageTime)) {
+    // Not enough messages or time span to calculate a frequency
+    return EM_DASH;
+  }
+  const topicDurationSec = toSec(subtractTimes(lastMessageTime, firstMessageTime));
+  return `${((numMessages - 1) / topicDurationSec).toFixed(2)} Hz`;
+}
+
+const selectStartTime = ({ playerState }: MessagePipelineContext) =>
+  playerState.activeData?.startTime;
+const selectEndTime = ({ playerState }: MessagePipelineContext) => playerState.activeData?.endTime;
+
+const selectTopicStats = (ctx: MessagePipelineContext) =>
+  ctx.playerState.activeData?.topicStats ?? EMPTY_TOPIC_STATS;
+
+/**
+ * Encapsulates logic for directly updating topic stats DOM elements, bypassing
+ * react for performance. To use this component mount it directly under your component
+ * containing topics you want to update. Tag each topic stat field with data-topic
+ * and data-topic-stat attributes.
+ *
+ * @property interval - the interval, in frames, between updates.
+ */
+export function DirectTopicStatsUpdater({ interval = 1 }: { interval?: number }): JSX.Element {
+  const startTime = useMessagePipeline(selectStartTime);
+  const endTime = useMessagePipeline(selectEndTime);
+  const topicStats = useMessagePipeline(selectTopicStats);
+  const duration = endTime && startTime ? subtractTimes(endTime, startTime) : { sec: 0, nsec: 0 };
+
+  const latestDuration = useLatest(duration);
+  const latestStats = useLatest(topicStats);
+  const updateCount = useRef(0);
+  const rootRef = useRef<HTMLDivElement>(ReactNull);
+
+  const updateStats = useCallback(() => {
+    if (!rootRef.current) {
+      return;
+    }
+
+    rootRef.current.parentElement?.querySelectorAll("[data-topic]").forEach((field) => {
+      if (field instanceof HTMLElement && field.dataset.topic) {
+        const topic = field.dataset.topic;
+        const stat = latestStats.current.get(topic);
+        if (field.dataset.topicStat === "count") {
+          if (stat) {
+            field.innerText = stat.numMessages.toLocaleString();
+          } else {
+            field.innerText = EM_DASH;
+          }
+        }
+        if (field.dataset.topicStat === "frequency") {
+          if (stat) {
+            field.innerText = formatItemFrequency(
+              stat.numMessages,
+              stat.firstMessageTime,
+              stat.lastMessageTime,
+              latestDuration.current,
+            );
+          } else {
+            field.innerText = EM_DASH;
+          }
+        }
+      }
+    });
+  }, [latestDuration, latestStats]);
+
+  useEffect(() => {
+    if (updateCount.current++ % interval === 0) {
+      updateStats();
+    }
+  }, [updateStats, interval, topicStats]);
+
+  useLayoutEffect(() => {
+    updateStats();
+  }, [updateStats]);
+
+  return <div ref={rootRef} style={{ display: "none" }} />;
+}

packages/studio-base/src/components/Duration.stories.tsx
@@ -1,57 +1,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-import { Stack } from "@mui/material";
-import { ComponentProps, useState } from "react";
-
-import { AppSetting } from "@foxglove/studio-base/AppSetting";
-import AppConfigurationContext from "@foxglove/studio-base/context/AppConfigurationContext";
-import { makeMockAppConfiguration } from "@foxglove/studio-base/util/makeMockAppConfiguration";
-
-import Duration from "./Duration";
-
-export default {
-  component: Duration,
-  title: "components/Duration",
-};
-
-type Props = ComponentProps<typeof Duration>;
-
-function DurationStory(props: Props): JSX.Element {
-  const [secAppConfig] = useState(() =>
-    makeMockAppConfiguration([
-      [AppSetting.TIME_FORMAT, "SEC"],
-      [AppSetting.TIMEZONE, "UTC"],
-    ]),
-  );
-  const [todAppConfig] = useState(() =>
-    makeMockAppConfiguration([
-      [AppSetting.TIME_FORMAT, "TOD"],
-      [AppSetting.TIMEZONE, "UTC"],
-    ]),
-  );
-
-  return (
-    <Stack padding={2} spacing={2}>
-      <AppConfigurationContext.Provider value={secAppConfig}>
-        <Duration {...props} />
-      </AppConfigurationContext.Provider>
-      <AppConfigurationContext.Provider value={todAppConfig}>
-        <Duration {...props} />
-      </AppConfigurationContext.Provider>
-    </Stack>
-  );
-}
-
-export function Zero(): JSX.Element {
-  return <DurationStory duration={{ sec: 0, nsec: 0 }} />;
-}
-
-export function Hour(): JSX.Element {
-  return <DurationStory duration={{ sec: 3600, nsec: 123456789 }} />;
-}
-
-export function HourMinuteSeconds(): JSX.Element {
-  return <DurationStory duration={{ sec: 23485, nsec: 123456789 }} />;
-}

packages/studio-base/src/components/Duration.tsx
@@ -1,35 +1,0 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/
-
-import { Typography } from "@mui/material";
-import { useMemo } from "react";
-
-import { Time } from "@foxglove/rostime";
-import Stack from "@foxglove/studio-base/components/Stack";
-import { useAppTimeFormat } from "@foxglove/studio-base/hooks";
-import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
-
-type Props = {
-  duration: Time;
-};
-
-/**
- * Duration component renders a formatted duration { sec, nsec } value according to the current
- * app time format setting.
- *
- * */
-export default function Duration(props: Props): JSX.Element {
-  const { duration } = props;
-  const { formatDuration } = useAppTimeFormat();
-
-  const durationStr = useMemo(() => formatDuration(duration), [duration, formatDuration]);
-
-  return (
-    <Stack direction="row" alignItems="center" flexGrow={0}>
-      <Typography variant="inherit" fontFamily={fonts.MONOSPACE}>
-        {durationStr}
-      </Typography>
-    </Stack>
-  );
-}

flora-suite/flora

commit 1af86ea10a03e01fcb34098fa802f82d91a1cf50

Fix datasource info view end time and duration (#4273)
This fixes a regression from https://github.com/foxglove/studio/pull/4238. The issue is that non-live data sources don't send multiple updates for the session end time so for these data sources we also need to update when the player presence changes in order to display the correct end time and duration.


Former-commit-id: fef3ff94efbd96090fb24a50f5ec5d3de8745afb

packages/studio-base/src/components/DataSourceInfoView.tsx
@@ -4,6 +4,7 @@
 
 import { Skeleton, Typography } from "@mui/material";
 import { MutableRefObject, useEffect, useRef } from "react";
+import { makeStyles } from "tss-react/mui";
 
 import { Time } from "@foxglove/rostime";
 import {
@@ -16,8 +17,15 @@ import { useAppTimeFormat } from "@foxglove/studio-base/hooks";
 import { subtractTimes } from "@foxglove/studio-base/players/UserNodePlayer/nodeTransformerWorker/typescript/userUtils/time";
 import { PlayerPresence } from "@foxglove/studio-base/players/types";
 import { formatDate, formatDuration } from "@foxglove/studio-base/util/formatTime";
+import { fonts } from "@foxglove/studio-base/util/sharedStyleConstants";
 
 import { MultilineMiddleTruncate } from "./MultilineMiddleTruncate";
+
+const useStyles = makeStyles()({
+  numericValue: {
+    fontFamily: fonts.MONOSPACE,
+  },
+});
 
 const selectStartTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.startTime;
 const selectEndTime = (ctx: MessagePipelineContext) => ctx.playerState.activeData?.endTime;
@@ -32,6 +40,7 @@ function DataSourceInfoContent(props: {
   startTime?: Time;
 }): JSX.Element {
   const { durationRef, endTimeRef, playerName, playerPresence, startTime } = props;
+  const { classes } = useStyles();
 
   return (
     <Stack gap={1.5} paddingX={2} paddingBottom={2}>
@@ -50,7 +59,9 @@ function DataSourceInfoContent(props: {
             <MultilineMiddleTruncate text={playerName} />
           </Typography>
         ) : (
-          <Typography>&mdash;</Typography>
+          <Typography className={classes.numericValue} variant="inherit">
+            &mdash;
+          </Typography>
         )}
       </Stack>
 
@@ -63,7 +74,9 @@ function DataSourceInfoContent(props: {
         ) : startTime ? (
           <Timestamp horizontal time={startTime} />
         ) : (
-          <Typography color="text.secondary">&mdash;</Typography>
+          <Typography className={classes.numericValue} variant="inherit" color="text.secondary">
+            &mdash;
+          </Typography>
         )}
       </Stack>
 
@@ -74,7 +87,7 @@ function DataSourceInfoContent(props: {
         {playerPresence === PlayerPresence.INITIALIZING ? (
           <Skeleton animation="wave" width="50%" />
         ) : (
-          <Typography variant="inherit" ref={endTimeRef}>
+          <Typography className={classes.numericValue} variant="inherit" ref={endTimeRef}>
             &mdash;
           </Typography>
         )}
@@ -87,7 +100,7 @@ function DataSourceInfoContent(props: {
         {playerPresence === PlayerPresence.INITIALIZING ? (
           <Skeleton animation="wave" width={100} />
         ) : (
-          <Typography variant="inherit" ref={durationRef}>
+          <Typography className={classes.numericValue} variant="inherit" ref={durationRef}>
             &mdash;
           </Typography>
         )}
@@ -125,10 +138,10 @@ export function DataSourceInfoView(): JSX.Element {
         const date = formatDate(endTime, undefined);
         endTimeRef.current.innerText = `${date} ${formatTime(endTime)}`;
       } else {
-        endTimeRef.current.innerText = EmDash;
+        endTimeRef.current.innerHTML = EmDash;
       }
     }
-  }, [endTime, formatTime, startTime]);
+  }, [endTime, formatTime, startTime, playerPresence]);
 
   return (
     <MemoDataSourceInfoContent