Lichtblick-Suite/lichtblick

commit b9d64e45a579096113ec429931fa6d7fdfd22301

[New3D] Use THREE.OrbitControls (#3926)

packages/studio-base/src/panels/ThreeDeeRender/Renderer.ts
@@ -5,6 +5,7 @@
 import EventEmitter from "eventemitter3";
 import { Immutable, produce } from "immer";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { DeepPartial } from "ts-essentials";
 import { v4 as uuidv4 } from "uuid";
 
@@ -218,6 +219,7 @@ export class Renderer extends EventEmitter<RendererEvents> {
   perspectiveCamera: THREE.PerspectiveCamera;
   orthographicCamera: THREE.OrthographicCamera;
   aspect: number;
+  controls: OrbitControls;
 
   // Are we connected to a ROS data source? Normalize coordinate frames if so by
   // stripping any leading "/" prefix. See `normalizeFrameId()` for details.
@@ -240,6 +242,7 @@ export class Renderer extends EventEmitter<RendererEvents> {
 
   private _prevResolution = new THREE.Vector2();
   private _pickingEnabled = false;
+  private _isUpdatingCameraState = false;
 
   constructor(canvas: HTMLCanvasElement, config: RendererConfig) {
     super();
@@ -310,6 +313,23 @@ export class Renderer extends EventEmitter<RendererEvents> {
     this.perspectiveCamera = new THREE.PerspectiveCamera();
     this.orthographicCamera = new THREE.OrthographicCamera();
 
+    this.controls = new OrbitControls(this.perspectiveCamera, this.canvas);
+    this.controls.screenSpacePanning = false; // only allow panning in the XY plane
+    this.controls.mouseButtons.LEFT = THREE.MOUSE.PAN;
+    this.controls.mouseButtons.RIGHT = THREE.MOUSE.ROTATE;
+    this.controls.touches.ONE = THREE.TOUCH.DOLLY_PAN;
+    this.controls.touches.TWO = THREE.TOUCH.ROTATE;
+    this.controls.addEventListener("change", () => {
+      if (!this._isUpdatingCameraState) {
+        this.emit("cameraMove", this);
+      }
+    });
+
+    // Make the canvas able to receive keyboard events and setup WASD controls
+    canvas.tabIndex = 1000;
+    this.controls.keys = { LEFT: "KeyA", RIGHT: "KeyD", UP: "KeyW", BOTTOM: "KeyS" };
+    this.controls.listenToKeyEvents(canvas);
+
     this.input = new Input(canvas, () => this.activeCamera());
     this.input.on("resize", (size) => this.resizeHandler(size));
     this.input.on("click", (cursorCoords) => this.clickHandler(cursorCoords));
@@ -370,6 +390,7 @@ export class Renderer extends EventEmitter<RendererEvents> {
     this.settings.off("update");
     this.input.off("resize", this.resizeHandler);
     this.input.off("click", this.clickHandler);
+    this.controls.dispose();
 
     for (const extension of this.sceneExtensions.values()) {
       extension.dispose();
@@ -593,6 +614,10 @@ export class Renderer extends EventEmitter<RendererEvents> {
   /** Translate a @foxglove/regl-worldview CameraState to the three.js coordinate system */
   private _updateCameras(cameraState: CameraState): void {
     if (cameraState.perspective) {
+      // Unlock the polar angle (pitch axis)
+      this.controls.minPolarAngle = 0;
+      this.controls.maxPolarAngle = Math.PI;
+
       this.perspectiveCamera.position
         .setFromSpherical(
           tempSpherical.set(cameraState.distance, cameraState.phi, -cameraState.thetaOffset),
@@ -613,7 +638,16 @@ export class Renderer extends EventEmitter<RendererEvents> {
       this.perspectiveCamera.far = cameraState.far;
       this.perspectiveCamera.aspect = this.aspect;
       this.perspectiveCamera.updateProjectionMatrix();
+      this.controls.target.set(
+        cameraState.targetOffset[0],
+        cameraState.targetOffset[1],
+        cameraState.targetOffset[2],
+      );
     } else {
+      // Lock the polar angle during 2D mode
+      const curPolarAngle = this.controls.getPolarAngle();
+      this.controls.minPolarAngle = this.controls.maxPolarAngle = curPolarAngle;
+
       this.orthographicCamera.position.set(
         cameraState.targetOffset[0],
         cameraState.targetOffset[1],
@@ -634,8 +668,25 @@ export class Renderer extends EventEmitter<RendererEvents> {
   }
 
   setCameraState(cameraState: CameraState): void {
+    this._isUpdatingCameraState = true;
     this._updateCameras(cameraState);
-    this.emit("cameraMove", this);
+    this.controls.update();
+    this._isUpdatingCameraState = false;
+  }
+
+  getCameraState(): CameraState {
+    return {
+      perspective: this.config.cameraState.perspective,
+      distance: this.controls.getDistance(),
+      phi: this.controls.getPolarAngle(),
+      thetaOffset: -this.controls.getAzimuthalAngle(),
+      targetOffset: [this.controls.target.x, this.controls.target.y, this.controls.target.z],
+      target: this.config.cameraState.target,
+      targetOrientation: this.config.cameraState.targetOrientation,
+      fovy: this.config.cameraState.fovy,
+      near: this.config.cameraState.near,
+      far: this.config.cameraState.far,
+    };
   }
 
   setSelectedRenderable(selectedRenderable: Renderable | undefined): void {

packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
@@ -16,19 +16,12 @@ import {
 import { isEqual, cloneDeep, merge } from "lodash";
 import React, { useCallback, useLayoutEffect, useEffect, useState, useMemo, useRef } from "react";
 import ReactDOM from "react-dom";
-import { useResizeDetector } from "react-resize-detector";
 import { useLatest, useLongPress } from "react-use";
 import { DeepPartial } from "ts-essentials";
 import { useDebouncedCallback } from "use-debounce";
 
 import Logger from "@foxglove/log";
-import {
-  CameraListener,
-  CameraState,
-  CameraStore,
-  DEFAULT_CAMERA_STATE,
-  MouseEventObject,
-} from "@foxglove/regl-worldview";
+import { CameraState, DEFAULT_CAMERA_STATE, MouseEventObject } from "@foxglove/regl-worldview";
 import { definitions as commonDefs } from "@foxglove/rosmsg-msgs-common";
 import { fromDate, toNanoSec } from "@foxglove/rostime";
 import {
@@ -419,10 +412,15 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
   );
 
   // Config cameraState
-  const setCameraState = useCallback((state: CameraState) => {
-    setConfig((prevConfig) => ({ ...prevConfig, cameraState: state }));
-  }, []);
-  const [cameraStore] = useState(() => new CameraStore(setCameraState, cameraState));
+  useEffect(() => {
+    const listener = () => {
+      if (renderer) {
+        setConfig((prevConfig) => ({ ...prevConfig, cameraState: renderer.getCameraState() }));
+      }
+    };
+    renderer?.addListener("cameraMove", listener);
+    return () => void renderer?.removeListener("cameraMove", listener);
+  }, [renderer]);
 
   // Build a map from topic name to datatype
   const topicsToDatatypes = useMemo(() => {
@@ -640,10 +638,9 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
 
   // Update the renderer when the camera moves
   useEffect(() => {
-    cameraStore.setCameraState(cameraState);
     renderer?.setCameraState(cameraState);
     renderRef.current.needsRender = true;
-  }, [cameraState, cameraStore, renderer]);
+  }, [cameraState, renderer]);
 
   // Render a new frame if requested
   useEffect(() => {
@@ -657,18 +654,6 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
   useEffect(() => {
     renderDone?.();
   }, [renderDone]);
-
-  // Use a debounce and 0 refresh rate to avoid triggering a resize observation while handling
-  // an existing resize observation.
-  // https://github.com/maslianok/react-resize-detector/issues/45
-  const {
-    ref: resizeRef,
-    width,
-    height,
-  } = useResizeDetector({
-    refreshRate: 0,
-    refreshMode: "debounce",
-  });
 
   // Create a useCallback wrapper for adding a new panel to the layout, used to open the
   // "Raw Messages" panel from the object inspector
@@ -821,24 +806,16 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
 
   return (
     <ThemeProvider isDark={colorScheme === "dark"}>
-      <div style={PANEL_STYLE} ref={resizeRef} onKeyDown={onKeyDown}>
-        <CameraListener cameraStore={cameraStore} shiftKeys={true}>
-          <div
-            // This element forces CameraListener to fill its container. We need this instead of just
-            // the canvas since three.js manages the size of the canvas element and we use
-            // position:absolute
-            style={{ width, height }}
-          />
-          <canvas
-            ref={setCanvas}
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              ...((measureActive || publishActive) && { cursor: "crosshair" }),
-            }}
-          />
-        </CameraListener>
+      <div style={PANEL_STYLE} onKeyDown={onKeyDown}>
+        <canvas
+          ref={setCanvas}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            ...((measureActive || publishActive) && { cursor: "crosshair" }),
+          }}
+        />
         <RendererContext.Provider value={renderer}>
           <RendererOverlay
             canvas={canvas}

flora-suite/flora

commit 8d5f741110bed491ee3d3f64ca6cefed240ecc57

[New3D] Fix touchscreen controls (#4011)
**User-Facing Changes**
Added touchscreen support to the 3D (Beta) panel.

**Description**
Fixes https://github.com/foxglove/studio/issues/3677

Touches were broken in https://github.com/foxglove/studio/pull/3926 when
it merged. This is the real fix for
https://github.com/foxglove/studio/issues/3677.
Former-commit-id: a1e26e1c34b0f657364d01b44d15b95359458c9f

packages/studio-base/src/panels/ThreeDeeRender/Renderer.ts
@@ -383,8 +383,8 @@ export class Renderer extends EventEmitter<RendererEvents> {
     this.controls.screenSpacePanning = false; // only allow panning in the XY plane
     this.controls.mouseButtons.LEFT = THREE.MOUSE.PAN;
     this.controls.mouseButtons.RIGHT = THREE.MOUSE.ROTATE;
-    this.controls.touches.ONE = THREE.TOUCH.DOLLY_PAN;
-    this.controls.touches.TWO = THREE.TOUCH.ROTATE;
+    this.controls.touches.ONE = THREE.TOUCH.PAN;
+    this.controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
     this.controls.addEventListener("change", () => {
       if (!this._isUpdatingCameraState) {
         this.emit("cameraMove", this);