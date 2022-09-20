Lichtblick-Suite/lichtblick

commit 3c55fafcfffc36bae95d642fa08fe9f9e0eb1c9f

Support loading files which contain messages spanning decades (#4398)
The BufferedIterableSource is responsible for wrapping an
IIterableSource and performing "look ahead" fetching in the "background"
with the purpose of fetching upcoming data before the player actually
needs to read the message. The background loading logic would request a
message iterator for a fixed amount of time (the readahead window of 5
seconds) from the underlying source. As the player consumed messages
from the message iterator of the BufferedIterableSource instance the
background process would move forward and start new message iterators as
needed to fill the readahead buffer. It knows it can stop buffering new
data when it reads a message that is beyond the required time of the
readahead window.

When loading a file that contains a massive gap in time (decades), this
logic of reading a fixed amount of time fall apart. The
BufferedIterableSource effectively goes into a tight loop. It wants to
read 5 seconds into the buffer. To do this it keeps trying to read 5
second increments from the underlying data source but since the next
message is decades away this creates a tight loop.

I originally wrote the BufferedIterableSource to work in fixed
increments on the underlying source because the DataPlatformIterable
source would otherwise try to fetch too much data over the network too
soon. I now believe this is a problem specific to the
DataPlatformIterableSource and behavior that should be internalized
within that source rather than impacting how buffering is done for all
sources.

This change updates the BufferedIterableSource to create a single
messageIterator on the underlying source and read from that as necessary
to keep the readahead buffer filled to the appropriate time. This avoids
the tight loop because the messageIterator is responsible for producing
the next message from the source regardless of how far from the previous
message it might be.

packages/studio-base/src/players/IterablePlayer/foxglove-data-platform/DataPlatformIterableSource.ts
@@ -14,6 +14,8 @@ import {
   isLessThan,
   Time,
   toRFC3339String,
+  add as addTime,
+  compare,
 } from "@foxglove/rostime";
 import {
   PlayerProblem,
@@ -21,7 +23,7 @@ import {
   MessageEvent,
   TopicStats,
 } from "@foxglove/studio-base/players/types";
-import ConsoleApi from "@foxglove/studio-base/services/ConsoleApi";
+import ConsoleApi, { CoverageResponse } from "@foxglove/studio-base/services/ConsoleApi";
 import { RosDatatypes } from "@foxglove/studio-base/types/RosDatatypes";
 import { formatTimeRaw } from "@foxglove/studio-base/util/time";
 
@@ -58,6 +60,8 @@ export class DataPlatformIterableSource implements IIterableSource {
    */
   private _parsedChannelsByTopic = new Map<string, ParsedChannelAndEncodings[]>();
 
+  private _coverage: CoverageResponse[] = [];
+
   public constructor(options: DataPlatformIterableSourceOptions) {
     this._consoleApi = options.api;
     this._start = options.start;
@@ -86,6 +90,8 @@ export class DataPlatformIterableSource implements IIterableSource {
         )} and ${formatTimeRaw(this._end)}.`,
       );
     }
+
+    this._coverage = coverage;
 
     // Truncate start/end time to coverage range
     const coverageStart = minBy(coverage, (c) => c.start);
@@ -199,16 +205,63 @@ export class DataPlatformIterableSource implements IIterableSource {
     const streamStart = args.start ?? this._start;
     const streamEnd = clampTime(args.end ?? this._end, this._start, this._end);
 
-    const stream = streamMessages({
-      api,
-      parsedChannelsByTopic,
-      params: { deviceId, start: streamStart, end: streamEnd, topics: args.topics },
-    });
+    if (args.consumptionType === "full") {
+      const stream = streamMessages({
+        api,
+        parsedChannelsByTopic,
+        params: { deviceId, start: streamStart, end: streamEnd, topics: args.topics },
+      });
 
-    for await (const messages of stream) {
-      for (const message of messages) {
-        yield { connectionId: undefined, msgEvent: message, problem: undefined };
+      for await (const messages of stream) {
+        for (const message of messages) {
+          yield { connectionId: undefined, msgEvent: message, problem: undefined };
+        }
       }
+
+      return;
+    }
+
+    let localStart = streamStart;
+    let localEnd = clampTime(addTime(localStart, { sec: 5, nsec: 0 }), streamStart, streamEnd);
+
+    for (;;) {
+      const stream = streamMessages({
+        api,
+        parsedChannelsByTopic,
+        params: { deviceId, start: localStart, end: localEnd, topics: args.topics },
+      });
+
+      for await (const messages of stream) {
+        for (const message of messages) {
+          yield { connectionId: undefined, msgEvent: message, problem: undefined };
+        }
+      }
+
+      if (compare(localEnd, streamEnd) >= 0) {
+        return;
+      }
+
+      localStart = addTime(localEnd, { sec: 0, nsec: 1 });
+
+      // find the coverage item where localStart < end
+      // if that item's start > localStart, use that item's start as the localStart
+      for (const coverage of this._coverage) {
+        // if local start is less than the coverate end, then the start is either in the coverage region
+        // or in a gap.
+        const end = fromRFC3339String(coverage.end);
+        const start = fromRFC3339String(coverage.start);
+        if (!start || !end) {
+          continue;
+        }
+
+        if (compare(localStart, end) <= 0 && compare(localStart, start) < 0) {
+          localStart = start;
+          break;
+        }
+      }
+
+      localStart = clampTime(localStart, streamStart, streamEnd);
+      localEnd = clampTime(addTime(localStart, { sec: 5, nsec: 0 }), streamStart, streamEnd);
     }
   }
 

packages/studio-base/src/players/IterablePlayer/BlockLoader.test.ts
@@ -516,21 +516,25 @@ describe("BlockLoader", () => {
 
     expect(messageIteratorCallArgs).toEqual([
       {
+        consumptionType: "full",
         topics: ["a"],
         start: { sec: 0, nsec: 0 },
         end: { sec: 9, nsec: 0 },
       },
       {
+        consumptionType: "full",
         topics: ["a"],
         start: { sec: 4, nsec: 500000003 },
         end: { sec: 9, nsec: 0 },
       },
       {
+        consumptionType: "full",
         topics: ["a"],
         start: { sec: 1, nsec: 500000001 },
         end: { sec: 4, nsec: 500000002 },
       },
       {
+        consumptionType: "full",
         topics: ["a"],
         start: { sec: 7, nsec: 500000005 },
         end: { sec: 9, nsec: 0 },

packages/studio-base/src/players/IterablePlayer/BlockLoader.ts
@@ -229,6 +229,7 @@ export class BlockLoader {
         topics: Array.from(topicsToFetch),
         start: iteratorStartTime,
         end: iteratorEndTime,
+        consumptionType: "full",
       });
 
       let messagesByTopic: Record<string, MessageEvent<unknown>[]> = {};

packages/studio-base/src/players/IterablePlayer/BufferedIterableSource.test.ts
@@ -2,6 +2,8 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
+import { debounce } from "lodash";
+
 import { MessageEvent } from "@foxglove/studio";
 
 import { BufferedIterableSource } from "./BufferedIterableSource";
@@ -405,18 +407,24 @@ describe("BufferedIterableSource", () => {
 
     let signal = waiter(1);
 
-    let count = 0;
+    const debounceNotify = debounce(() => {
+      signal.notify();
+    }, 500);
+
+    let messageIteratorCount = 0;
     source.messageIterator = async function* messageIterator(
       args: MessageIteratorArgs,
     ): AsyncIterableIterator<Readonly<IteratorResult>> {
-      count += 1;
+      expect(args).toEqual({
+        topics: ["a"],
+        start: { sec: 0, nsec: 0 },
+        end: { sec: 10, nsec: 0 },
+        consumptionType: "partial",
+      });
+      messageIteratorCount += 1;
 
-      const start = args.start?.sec ?? 0;
-      const end = args.end?.sec ?? 1000;
       for (let i = 0; i < 8; ++i) {
-        if (i < start || i > end) {
-          continue;
-        }
+        debounceNotify();
         yield {
           msgEvent: {
             topic: "a",
@@ -428,7 +436,6 @@ describe("BufferedIterableSource", () => {
           connectionId: undefined,
         };
       }
-      signal.notify();
     };
 
     const messageIterator = bufferedSource.messageIterator({
@@ -438,23 +445,18 @@ describe("BufferedIterableSource", () => {
     // Reading the first message buffers some data
     await messageIterator.next();
 
-    // Wait for the producer to finish
+    // Wait for the buffered iterable source to stop reading messages
     await signal.wait();
 
-    expect(count).toEqual(1);
-    expect(bufferedSource.loadedRanges()).toEqual([{ start: 0, end: 0.2 }]);
+    expect(bufferedSource.loadedRanges()).toEqual([{ start: 0, end: 0.1999999999 }]);
 
-    // Reading the second message does not need to buffer any data because we still have enough data
-    // to read
-    await messageIterator.next();
-    expect(count).toEqual(1);
-    expect(bufferedSource.loadedRanges()).toEqual([{ start: 0, end: 0.2 }]);
-
-    // Reading the third message pushes us to buffer more data
+    // Reading the second message buffers more data
     signal = waiter(1);
     await messageIterator.next();
     await signal.wait();
-    expect(count).toEqual(2);
-    expect(bufferedSource.loadedRanges()).toEqual([{ start: 0, end: 0.4000000001 }]);
+    expect(bufferedSource.loadedRanges()).toEqual([{ start: 0, end: 0.2999999999 }]);
+
+    // We should have called the messageIterator method only once
+    expect(messageIteratorCount).toEqual(1);
   });
 });

packages/studio-base/src/players/IterablePlayer/BufferedIterableSource.ts
@@ -20,7 +20,7 @@ import {
 
 const log = Log.getLogger(__filename);
 
-const DEFAULT_READ_AHEAD_DURATION = { sec: 5, nsec: 0 };
+const DEFAULT_READ_AHEAD_DURATION = { sec: 10, nsec: 0 };
 
 type Options = {
   // How far ahead to buffer
@@ -88,65 +88,46 @@ class BufferedIterableSource implements IIterableSource {
     // the start of the array.
     this.cache.clear();
 
-    // Streaming starts where the read head is and adjust as data is buffered and read
-    let streamStart = this.readHead;
-
     try {
-      for (;;) {
+      const sourceIterator = this.source.messageIterator({
+        topics: args.topics,
+        start: this.readHead,
+        consumptionType: "partial",
+      });
+
+      // Messages are read from the source until reaching the readUntil time. Then we wait for the read head
+      // to move forward until it is within 1 headAheadDuration of the readUntil time until trying to read more.
+      // We set the readUntil time to 2x readAheadDuration so that every move of the read head does not require
+      // also reading the underlying source. This creates the effect of buffering some
+      let readUntil = clampTime(
+        addTime(this.readHead, this.readAheadDuration),
+        this.initResult.start,
+        this.initResult.end,
+      );
+
+      for await (const result of sourceIterator) {
         if (this.aborted) {
-          break;
-        }
-
-        const readUntil = addTime(
-          streamStart,
-          addTime(this.readAheadDuration, this.readAheadDuration),
-        );
-        const streamEnd = clampTime(readUntil, this.initResult.start, this.initResult.end);
-
-        const sourceIterator = this.source.messageIterator({
-          topics: args.topics,
-          start: streamStart,
-          end: streamEnd,
-        });
-
-        for await (const result of sourceIterator) {
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-          if (this.aborted) {
-            break;
-          }
-
-          if (result.msgEvent && compare(result.msgEvent.receiveTime, streamEnd) > 0) {
-            throw new Error("Invariant: out of bounds result");
-          }
-
-          this.cache.enqueue(result);
-
-          // Indicate to the consumer that it can try reading again
-          this.readSignal.notifyAll();
-        }
-
-        // We've streamed through the end of our data source
-        if (compare(streamEnd, this.initResult.end) >= 0) {
           return;
         }
 
-        // Wait until we've consumed enough data that we should read more
-        for (;;) {
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-          if (this.aborted) {
-            break;
-          }
+        this.cache.enqueue(result);
 
-          // If this.readHead + readAheadTime > streamEnd, we start another stream for buffering
-          // otherwise we wait
-          const targetUntil = addTime(this.readHead, this.readAheadDuration);
-          if (compare(targetUntil, streamEnd) > 0 || this.cache.size() === 0) {
-            streamStart = addTime(streamEnd, { sec: 0, nsec: 1 });
-            break;
-          }
+        // Indicate to the consumer that it can try reading again
+        this.readSignal.notifyAll();
 
-          await this.writeSignal.wait();
+        // Keep reading while the messages we receive are <= the readUntil time
+        if (result.msgEvent && compare(result.msgEvent.receiveTime, readUntil) <= 0) {
+          continue;
         }
+
+        // If we didn't load anything into the cache keep reading
+        if (this.cache.size() === 0) {
+          continue;
+        }
+
+        // Wait for messages to be read and then update the new readUntil
+        await this.writeSignal.wait();
+        readUntil = addTime(this.readHead, this.readAheadDuration);
       }
     } finally {
       // Indicate to the consumer that it can try reading again

packages/studio-base/src/players/IterablePlayer/CachingIterableSource.ts
@@ -207,6 +207,7 @@ class CachingIterableSource implements IIterableSource {
         topics: this.cachedTopics,
         start: sourceReadStart,
         end: sourceReadEnd,
+        consumptionType: args.consumptionType,
       });
 
       // The cache is indexed on time, but iterator results that are problems might not have a time.

packages/studio-base/src/players/IterablePlayer/IIterableSource.ts
@@ -41,6 +41,17 @@ export type MessageIteratorArgs = {
    * The last message receiveTime will be <= end.
    * */
   end?: Time;
+
+  /**
+   * Indicate the expected way the iterator is consumed.
+   *
+   * Data sources may choose to change internal mechanics depending on whether the messages are
+   * consumed immediate in full from the iterator or if it might be read partially.
+   *
+   * `full` indicates that the caller plans to read the entire iterator
+   * `partial` indicates that the caller plans to read the iterator but may not read all the messages
+   */
+  consumptionType?: "full" | "partial";
 };
 
 export type IteratorResult =

packages/studio-base/src/players/IterablePlayer/IterablePlayer.test.ts
@@ -382,8 +382,22 @@ describe("IterablePlayer", () => {
     await store.done;
 
     expect(messageIteratorSpy.mock.calls).toEqual([
-      [{ start: { sec: 0, nsec: 0 }, end: { sec: 1, nsec: 0 }, topics: ["foo"] }],
-      [{ start: { sec: 0, nsec: 99000001 }, end: { sec: 1, nsec: 0 }, topics: ["bar", "foo"] }],
+      [
+        {
+          start: { sec: 0, nsec: 0 },
+          end: { sec: 1, nsec: 0 },
+          topics: ["foo"],
+          consumptionType: "partial",
+        },
+      ],
+      [
+        {
+          start: { sec: 0, nsec: 99000001 },
+          end: { sec: 1, nsec: 0 },
+          topics: ["bar", "foo"],
+          consumptionType: "partial",
+        },
+      ],
     ]);
 
     player.close();

flora-suite/flora

commit 4be4d2f71157ea35290dff66256b3a66024439b8

Fix DataPlatformIterableSource partial consumption (#4449)
When streaming from data platform using the "partial" consumption type,
the iterable source would incorrectly reset the next window. This
happened when trying to use coverage information to skip gaps but
failing to detect that the window was already in a valid coverage zone.

Regressed in: https://github.com/foxglove/studio/pull/4398

Former-commit-id: af7f22c37e673f40f3b054406143914d7b44c3f4

packages/studio-base/src/players/IterablePlayer/foxglove-data-platform/DataPlatformIterableSource.ts
@@ -243,17 +243,22 @@ export class DataPlatformIterableSource implements IIterableSource {
 
       localStart = addTime(localEnd, { sec: 0, nsec: 1 });
 
-      // find the coverage item where localStart < end
-      // if that item's start > localStart, use that item's start as the localStart
+      // Assumes coverage regions are sorted by start time
       for (const coverage of this._coverage) {
-        // if local start is less than the coverate end, then the start is either in the coverage region
-        // or in a gap.
         const end = fromRFC3339String(coverage.end);
         const start = fromRFC3339String(coverage.start);
         if (!start || !end) {
           continue;
         }
 
+        // if localStart is in a coverage region, then allow this localStart to be used
+        if (compare(localStart, start) >= 0 && compare(localStart, end) <= 0) {
+          break;
+        }
+
+        // if localStart is completely before a coverage region then we reset the localStart to the
+        // start of the coverage region. Since coverage regions are sorted by start time, if we get
+        // here we know that localStart did not fall into a previous coverage region
         if (compare(localStart, end) <= 0 && compare(localStart, start) < 0) {
           localStart = start;
           break;