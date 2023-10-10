Lichtblick-Suite/lichtblick

commit 90c43651960eed479ca13648cdf2cecb56aec446

Inject custom scene extensions from `injectedFeatures` (#6894)
**User-Facing Changes**
<!-- will be used as a changelog entry -->
 - n/a

**Description**

Added `InjectedFeatures` which is an entrypoint that allows features to
be injected from an outside context.

Modified `ImageMode` and `Images` scene extensions and the
`ImageRenderable` class that they use to be more friendly to subclassing
to add support for additional schema types.


<!-- link relevant GitHub issues -->
<!-- add `docs` label if this PR requires documentation updates -->
<!-- add relevant metric tracking for experimental / new features -->

packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/ImageRenderable.ts
@@ -18,10 +18,11 @@ import { RosValue } from "@foxglove/studio-base/players/types";
 import { AnyImage } from "./ImageTypes";
 import { decodeCompressedImageToBitmap } from "./decodeImage";
 import { CameraInfo } from "../../ros";
+import { DECODE_IMAGE_ERR_KEY, IMAGE_TOPIC_PATH } from "../ImageMode/constants";
 import { ColorModeSettings } from "../colorMode";
 
 const log = Logger.getLogger(__filename);
-
+const IMAGE_FORMATS = new Set(["jpeg", "png", "webp"]);
 export interface ImageRenderableSettings extends Partial<ColorModeSettings> {
   visible: boolean;
   frameLocked?: boolean;
@@ -30,9 +31,6 @@ export interface ImageRenderableSettings extends Partial<ColorModeSettings> {
   planarProjectionFactor: number;
   color: string;
 }
-
-const DECODE_IMAGE_ERR_KEY = "CreateBitmap";
-const IMAGE_TOPIC_PATH = ["imageMode", "imageTopic"];
 
 const DEFAULT_DISTANCE = 1;
 const DEFAULT_PLANAR_PROJECTION_FACTOR = 0;
@@ -73,7 +71,7 @@ export class ImageRenderable extends Renderable<ImageUserData> {
   #isUpdating = false;
 
   #decodedImage?: ImageBitmap | ImageData;
-  #decoder?: WorkerImageDecoder;
+  protected decoder?: WorkerImageDecoder;
   #receivedImageSequenceNumber = 0;
   #displayedImageSequenceNumber = 0;
 
@@ -83,12 +81,21 @@ export class ImageRenderable extends Renderable<ImageUserData> {
     super(topicName, renderer, userData);
   }
 
+  protected isDisposed(): boolean {
+    return this.#disposed;
+  }
+
+  public setTopic(topicName: string): void {
+    this.name = topicName;
+    this.userData.topic = topicName;
+  }
+
   public override dispose(): void {
     this.#disposed = true;
     this.userData.texture?.dispose();
     this.userData.material?.dispose();
     this.userData.geometry?.dispose();
-    this.#decoder?.terminate();
+    this.decoder?.terminate();
     super.dispose();
   }
 
@@ -173,13 +180,11 @@ export class ImageRenderable extends Renderable<ImageUserData> {
     this.userData.image = image;
 
     const seq = ++this.#receivedImageSequenceNumber;
-    const decodePromise =
-      "format" in image
-        ? decodeCompressedImageToBitmap(image, resizeWidth)
-        : (this.#decoder ??= new WorkerImageDecoder()).decode(image, this.userData.settings);
+    const decodePromise = this.decodeImage(image, resizeWidth);
+
     decodePromise
       .then((result) => {
-        if (this.#disposed) {
+        if (this.isDisposed()) {
           return;
         }
         // prevent displaying an image older than the one currently displayed
@@ -192,26 +197,29 @@ export class ImageRenderable extends Renderable<ImageUserData> {
         this.update();
 
         onDecoded?.(result);
-        this.renderer.settings.errors.remove(IMAGE_TOPIC_PATH, DECODE_IMAGE_ERR_KEY);
-        this.renderer.settings.errors.removeFromTopic(this.userData.topic, DECODE_IMAGE_ERR_KEY);
+        this.removeError(DECODE_IMAGE_ERR_KEY);
         this.renderer.queueAnimationFrame();
       })
       .catch((err) => {
         log.error(err);
-        if (this.#disposed) {
+        if (this.isDisposed()) {
           return;
         }
-        this.renderer.settings.errors.add(
-          IMAGE_TOPIC_PATH,
-          DECODE_IMAGE_ERR_KEY,
-          `Error decoding image: ${err.message}`,
-        );
-        this.renderer.settings.errors.addToTopic(
-          this.userData.topic,
-          DECODE_IMAGE_ERR_KEY,
-          `Error decoding image: ${err.message}`,
-        );
+        this.addError(DECODE_IMAGE_ERR_KEY, `Error decoding image: ${err.message}`);
       });
+  }
+
+  protected async decodeImage(
+    image: AnyImage,
+    resizeWidth?: number,
+  ): Promise<ImageBitmap | ImageData> {
+    if ("format" in image) {
+      if (!IMAGE_FORMATS.has(image.format)) {
+        throw new Error(`Unsupported format: "${image.format}"`);
+      }
+      return await decodeCompressedImageToBitmap(image, resizeWidth);
+    }
+    return await (this.decoder ??= new WorkerImageDecoder()).decode(image, this.userData.settings);
   }
 
   public update(): void {
@@ -366,6 +374,20 @@ export class ImageRenderable extends Renderable<ImageUserData> {
     }
 
     this.userData.mesh.renderOrder = -1 * Number.MAX_SAFE_INTEGER;
+  }
+
+  protected addError(key: string, message: string): void {
+    if (this.isDisposed()) {
+      return;
+    }
+    // must account for if the renderable is part of `ImageMode` or `Images` scene extension
+    this.renderer.settings.errors.add(IMAGE_TOPIC_PATH, key, message);
+    this.renderer.settings.errors.addToTopic(this.userData.topic, key, message);
+  }
+
+  protected removeError(key: string): void {
+    this.renderer.settings.errors.remove(IMAGE_TOPIC_PATH, key);
+    this.renderer.settings.errors.removeFromTopic(this.userData.topic, key);
   }
 }
 

packages/studio-base/src/context/AppContext.ts
@@ -3,11 +3,13 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 import { createContext, useContext } from "react";
+import { DeepPartial } from "ts-essentials";
 import { StoreApi } from "zustand";
 
 import { AppBarMenuItem } from "@foxglove/studio-base/components/AppBar/types";
 import { LayoutData } from "@foxglove/studio-base/context/CurrentLayoutContext";
 import { WorkspaceContextStore } from "@foxglove/studio-base/context/Workspace/WorkspaceContext";
+import { SceneExtensionConfig } from "@foxglove/studio-base/panels/ThreeDeeRender/SceneExtensionConfig";
 
 interface IAppContext {
   appBarLayoutButton?: JSX.Element;
@@ -18,6 +20,7 @@ interface IAppContext {
     durationNanos: string;
     metadata: Record<string, string>;
   }) => Promise<void>;
+  injectedFeatures?: InjectedFeatures;
   importLayoutFile?: (fileName: string, data: LayoutData) => Promise<void>;
   layoutEmptyState?: JSX.Element;
   syncAdapters?: readonly JSX.Element[];
@@ -26,6 +29,20 @@ interface IAppContext {
     initialState?: Partial<WorkspaceContextStore>,
   ) => StoreApi<WorkspaceContextStore>;
 }
+
+export const INJECTED_FEATURE_KEYS = {
+  customSceneExtensions: "ThreeDeeRender.customSceneExtensions",
+} as const;
+
+export type InjectedFeatureMap = {
+  [INJECTED_FEATURE_KEYS.customSceneExtensions]?: {
+    customSceneExtensions: DeepPartial<SceneExtensionConfig>;
+  };
+};
+
+export type InjectedFeatures = {
+  availableFeatures: InjectedFeatureMap;
+};
 
 const AppContext = createContext<IAppContext>({});
 AppContext.displayName = "AppContext";

packages/studio-base/src/panels/ThreeDeeRender/LayerErrors.ts
@@ -22,7 +22,7 @@ export class NodeError {
   public errorMessage(): string | undefined {
     if (this.errorsById && this.errorsById.size > 0) {
       const errorMessages = Array.from(this.errorsById.values());
-      return errorMessages.join(`, `);
+      return errorMessages.join(`\n`);
     } else {
       return undefined;
     }

packages/studio-base/src/panels/ThreeDeeRender/Renderer.ts
@@ -31,7 +31,6 @@ import {
   BuiltinPanelExtensionContext,
 } from "@foxglove/studio-base/components/PanelExtensionAdapter";
 import { LayerErrors } from "@foxglove/studio-base/panels/ThreeDeeRender/LayerErrors";
-import { SceneExtensionConfig } from "@foxglove/studio-base/panels/ThreeDeeRender/SceneExtensionConfig";
 import { ICameraHandler } from "@foxglove/studio-base/panels/ThreeDeeRender/renderables/ICameraHandler";
 import IAnalytics from "@foxglove/studio-base/services/IAnalytics";
 import { palette, fontMonospace } from "@foxglove/theme";
@@ -50,6 +49,7 @@ import { DEFAULT_MESH_UP_AXIS, ModelCache } from "./ModelCache";
 import { PickedRenderable, Picker } from "./Picker";
 import type { Renderable } from "./Renderable";
 import { SceneExtension } from "./SceneExtension";
+import { SceneExtensionConfig } from "./SceneExtensionConfig";
 import { ScreenOverlay } from "./ScreenOverlay";
 import { SettingsManager, SettingsTreeEntry } from "./SettingsManager";
 import { SharedGeometry } from "./SharedGeometry";
@@ -215,10 +215,10 @@ export class Renderer extends EventEmitter<RendererEvents> implements IRenderer 
     canvas: HTMLCanvasElement;
     config: Immutable<RendererConfig>;
     interfaceMode: InterfaceMode;
+    sceneExtensionConfig: SceneExtensionConfig;
     fetchAsset: BuiltinPanelExtensionContext["unstable_fetchAsset"];
     displayTemporaryError?: (message: string) => void;
     testOptions: TestOptions;
-    sceneExtensionConfig: SceneExtensionConfig;
   }) {
     super();
     this.displayTemporaryError = args.displayTemporaryError;

packages/studio-base/src/panels/ThreeDeeRender/SceneExtension.ts
@@ -105,9 +105,9 @@ export class SceneExtension<
   }
 
   /** Allows SceneExtensions to add options to the context menu. */
-  public getContextMenuItems = (): readonly PanelContextMenuItem[] => {
+  public getContextMenuItems(): readonly PanelContextMenuItem[] {
     return [];
-  };
+  }
 
   /**
    * Returns a list of settings nodes generated by this extension and the paths they appear at in

packages/studio-base/src/panels/ThreeDeeRender/foxglove.ts
@@ -40,7 +40,7 @@ addFoxgloveSchema(IMAGE_ANNOTATIONS_DATATYPES, "foxglove.ImageAnnotations");
 
 // Expand a single Foxglove schema name into variations for ROS1, ROS2, and IDL and add
 // them to the output set.
-function addFoxgloveSchema(output: Set<string>, dataType: string): Set<string> {
+export function addFoxgloveSchema(output: Set<string>, dataType: string): Set<string> {
   // Add the Foxglove json, protobuf, and flatbuffer variation: foxglove.PointCloud
   output.add(dataType);
 

packages/studio-base/src/panels/ThreeDeeRender/index.tsx
@@ -4,6 +4,7 @@
 
 import { StrictMode, useMemo } from "react";
 import ReactDOM from "react-dom";
+import { DeepPartial } from "ts-essentials";
 
 import { useCrash } from "@foxglove/hooks";
 import { CaptureErrorBoundary } from "@foxglove/studio-base/components/CaptureErrorBoundary";
@@ -17,9 +18,11 @@ import {
   BuiltinPanelExtensionContext,
   PanelExtensionAdapter,
 } from "@foxglove/studio-base/components/PanelExtensionAdapter";
+import { INJECTED_FEATURE_KEYS, useAppContext } from "@foxglove/studio-base/context/AppContext";
 import { TestOptions } from "@foxglove/studio-base/panels/ThreeDeeRender/IRenderer";
 import { SaveConfig } from "@foxglove/studio-base/types/panels";
 
+import { SceneExtensionConfig } from "./SceneExtensionConfig";
 import { ThreeDeeRender } from "./ThreeDeeRender";
 import { InterfaceMode } from "./types";
 
@@ -28,10 +31,11 @@ type InitPanelArgs = {
   forwardedAnalytics: ForwardedAnalytics;
   interfaceMode: InterfaceMode;
   testOptions: TestOptions;
+  customSceneExtensions?: DeepPartial<SceneExtensionConfig>;
 };
 
 function initPanel(args: InitPanelArgs, context: BuiltinPanelExtensionContext) {
-  const { crash, forwardedAnalytics, interfaceMode, testOptions } = args;
+  const { crash, forwardedAnalytics, interfaceMode, testOptions, customSceneExtensions } = args;
   // eslint-disable-next-line react/no-deprecated
   ReactDOM.render(
     <StrictMode>
@@ -41,6 +45,7 @@ function initPanel(args: InitPanelArgs, context: BuiltinPanelExtensionContext) {
             context={context}
             interfaceMode={interfaceMode}
             testOptions={testOptions}
+            customSceneExtensions={customSceneExtensions}
           />
         </ForwardAnalyticsContextProvider>
       </CaptureErrorBoundary>
@@ -64,6 +69,17 @@ function ThreeDeeRenderAdapter(interfaceMode: InterfaceMode, props: Props) {
   const crash = useCrash();
 
   const forwardedAnalytics = useForwardAnalytics();
+  const { injectedFeatures } = useAppContext();
+  const customSceneExtensions = useMemo(() => {
+    if (injectedFeatures == undefined) {
+      return undefined;
+    }
+    const injectedSceneExtensions =
+      injectedFeatures.availableFeatures[INJECTED_FEATURE_KEYS.customSceneExtensions]
+        ?.customSceneExtensions;
+    return injectedSceneExtensions;
+  }, [injectedFeatures]);
+
   const boundInitPanel = useMemo(
     () =>
       initPanel.bind(undefined, {
@@ -71,8 +87,16 @@ function ThreeDeeRenderAdapter(interfaceMode: InterfaceMode, props: Props) {
         forwardedAnalytics,
         interfaceMode,
         testOptions: { onDownloadImage: props.onDownloadImage, debugPicking: props.debugPicking },
+        customSceneExtensions,
       }),
-    [crash, forwardedAnalytics, interfaceMode, props.onDownloadImage, props.debugPicking],
+    [
+      crash,
+      forwardedAnalytics,
+      interfaceMode,
+      props.onDownloadImage,
+      props.debugPicking,
+      customSceneExtensions,
+    ],
   );
 
   return (

flora-suite/flora

commit 4e5fba6693ac435554d1b4341dc949e939a8aa46

Fix detection of image formats (#6974)
**User-Facing Changes**
<!-- will be used as a changelog entry -->

**Description**
Image formats are not always just `jpeg` they can be "mono8; jpeg
compressed". We used to support this, it regressed in
https://github.com/foxglove/studio/pull/6894


<!-- link relevant GitHub issues -->
<!-- add `docs` label if this PR requires documentation updates -->
<!-- add relevant metric tracking for experimental / new features -->
Former-commit-id: 8aa80e1aa1531f0ffa17bf7070a05adcceb85660

packages/studio-base/src/panels/ThreeDeeRender/renderables/Images/ImageRenderable.ts
@@ -22,7 +22,6 @@ import { DECODE_IMAGE_ERR_KEY, IMAGE_TOPIC_PATH } from "../ImageMode/constants";
 import { ColorModeSettings } from "../colorMode";
 
 const log = Logger.getLogger(__filename);
-const IMAGE_FORMATS = new Set(["jpeg", "png", "webp"]);
 export interface ImageRenderableSettings extends Partial<ColorModeSettings> {
   visible: boolean;
   frameLocked?: boolean;
@@ -214,9 +213,6 @@ export class ImageRenderable extends Renderable<ImageUserData> {
     resizeWidth?: number,
   ): Promise<ImageBitmap | ImageData> {
     if ("format" in image) {
-      if (!IMAGE_FORMATS.has(image.format)) {
-        throw new Error(`Unsupported format: "${image.format}"`);
-      }
       return await decodeCompressedImageToBitmap(image, resizeWidth);
     }
     return await (this.decoder ??= new WorkerImageDecoder()).decode(image, this.userData.settings);