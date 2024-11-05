9164fd6be65df7d17fbc8895a259668c708827d6
flora <leij59279@gmail.com>
2024-11-05 22:54:12 +0800

    Created unit test PathLegends component in StateTransitions
    **User-Facing Changes**
    N/A
    
    **Description**
    Created unit test for the PathLegends component in the State Transitions
    panel.
    
    **Checklist**
    
    - [x] The web version was tested and it is running ok
    - [x] The desktop version was tested and it is running ok
    - [x] This change is covered by unit tests
    
    
    Former-commit-id: f8c6901be55f10413587c60f3928b571fabfa70c

--- packages/suite-base/src/i18n/en/stateTransitions.ts
+++ packages/suite-base/src/i18n/en/stateTransitions.ts
@@ -3,6 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 export const stateTransitions = {
+  addSeriesButton: "Click to add a series",
   max: "Max",
   maxXError: "X max must be greater than X min.",
   min: "Min",
