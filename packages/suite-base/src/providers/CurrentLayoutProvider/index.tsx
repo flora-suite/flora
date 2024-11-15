152d756b9c6968112c28c184647aeeeee26af006
flora <leij59279@gmail.com>
2024-11-16 01:37:00 +0800

    fixed sync camera fial issue

--- packages/suite-base/src/providers/CurrentLayoutProvider/index.tsx
+++ packages/suite-base/src/providers/CurrentLayoutProvider/index.tsx
@@ -293,9 +293,23 @@ export default function CurrentLayoutProvider({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getUserProfile, layoutManager, setSelectedLayoutId]);
 
+  const updateSharedPanelState = useCallback<ICurrentLayout["actions"]["updateSharedPanelState"]>(
+    (type, newSharedState) => {
+      if (layoutStateRef.current.selectedLayout?.data == undefined) {
+        return;
+      }
+
+      setLayoutState({
+        ...layoutStateRef.current,
+        sharedPanelState: { ...layoutStateRef.current.sharedPanelState, [type]: newSharedState },
+      });
+    },
+    [setLayoutState],
+  );
+
   const actions: ICurrentLayout["actions"] = useMemo(
     () => ({
-      updateSharedPanelState: () => { },
+      updateSharedPanelState,
       setCurrentLayout: () => { },
       setSelectedLayoutId,
       getCurrentLayoutState: () => layoutStateRef.current,
@@ -386,7 +400,7 @@ export default function CurrentLayoutProvider({
         performAction({ type: "END_DRAG", payload });
       },
     }),
-    [analytics, performAction, setSelectedLayoutId, setSelectedPanelIds],
+    [analytics, performAction, setSelectedLayoutId, setSelectedPanelIds, updateSharedPanelState],
   );
 
   const value: ICurrentLayout = useShallowMemo({
